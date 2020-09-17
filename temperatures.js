66511fc66ab6be1a782a3e33ef594ca98a0db50a
Luthfi Anandra <62977278+lanandra@users.noreply.github.com>
2020-09-18 00:10:44 +0700

    Update temperatures.js

--- temperatures.js
+++ temperatures.js
@@ -1,18 +1,22 @@
-//set kelvin temperature
+//set Kelvin temperature
 const kelvin = 0;
 
-//set celcius temperature
+//set Celcius temperature
 const celcius = kelvin - 273;
 
-//set fahrenheit temperature
+//set Fahrenheit temperature
 let fahrenheit = celcius * (9 / 5) + 32;
 //round fahrenheit
 fahrenheit = Math.floor(fahrenheit);
 
+//print Kelvin, Celcius and Fahrenheit Temperatures
 console.log(`The temperature is ${kelvin} degrees Kelvin`);
 console.log(`The temperature is ${celcius} degrees Celcius`);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
 
+//set Newton temperature
 let newton = celcius * (33 / 100);
 newton = Math.floor(newton);
+
+//print Newton temperature
 console.log(`The temperature is ${newton} degrees Newton`);
