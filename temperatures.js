c3799448030e38c144aecf8918cfcaf26e783915
Luthfi Anandra <62977278+lanandra@users.noreply.github.com>
2020-09-18 00:22:56 +0700

    Update temperatures.js

--- temperatures.js
+++ temperatures.js
@@ -1,8 +1,13 @@
-//set Kelvin temperature
-const kelvin = 0;
+/*
+This temperature converter is used celcius as base temperature.
+Changing the value on celcius temperature, then it will convert the value of other temperatures
+*/
 
 //set Celcius temperature
-const celcius = kelvin - 273;
+const celcius = 0;
+
+//set Kelvin temperature
+const kelvin = celcius + 273;
 
 //set Fahrenheit temperature
 let fahrenheit = celcius * (9 / 5) + 32;
@@ -10,12 +15,13 @@ let fahrenheit = celcius * (9 / 5) + 32;
 fahrenheit = Math.floor(fahrenheit);
 
 //print Kelvin, Celcius and Fahrenheit Temperatures
-console.log(`The temperature is ${kelvin} degrees Kelvin`);
 console.log(`The temperature is ${celcius} degrees Celcius`);
+console.log(`The temperature is ${kelvin} degrees Kelvin`);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
 
 //set Newton temperature
 let newton = celcius * (33 / 100);
+//round Newton
 newton = Math.floor(newton);
 
 //print Newton temperature
